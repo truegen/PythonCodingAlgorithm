70f09605efedc90ced835777aa9762e39769d2de
truegen <truegen@gmail.com>
2018-05-07 15:28:19 +0900

    first commit

---
+++ 2-4.두 정수를 입력받고 뺀 값 출력하기.py
@@ -0,0 +1,14 @@
+def isNumStr(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+num1 = input("정수 숫자를 입력하세요: ")
+num2 = input("다음 정수를 입력하세요: ")
+
+if isNumStr(num1) and isNumStr(num2):
+    print(int(num1)-int(num2))
+else:
+    print("정수가 아닌 것이 포함되어 있습니다.")
