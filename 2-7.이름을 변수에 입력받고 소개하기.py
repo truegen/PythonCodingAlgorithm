0b300a8d148633dd9144783c60f92cae10d49122
truegen <truegen@gmail.com>
2018-05-07 16:15:50 +0900

    add comment

--- 2-7.이름을 변수에 입력받고 소개하기.py
+++ 2-7.이름을 변수에 입력받고 소개하기.py
@@ -1,7 +1,7 @@
 name = input("이름을 입력하세요: ")
-n = name.strip()
+n = name.strip()    # 이름 앞뒤 공백 제거
 
-if(n != ""):
+if(n != ""):    # 입력된 이름이 있으면
     print("입력한 이름은 %s입니다." %n)
 else:
     print("입력한 이름이 없습니다.")
