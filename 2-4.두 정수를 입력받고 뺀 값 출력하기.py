7d74ea00be3b0e4d2722946e09404eef9fe11379
truegen <truegen@gmail.com>
2018-05-07 17:27:37 +0900

    add function

--- 2-4.두 정수를 입력받고 뺀 값 출력하기.py
+++ 2-4.두 정수를 입력받고 뺀 값 출력하기.py
@@ -1,4 +1,4 @@
-def isNumStr(s):
+def isNumStr(s):    # 정수인지 검사
     try:
         int(s)
         return True
@@ -12,3 +12,10 @@ if isNumStr(num1) and isNumStr(num2):
     print(int(num1)-int(num2))
 else:
     print("정수가 아닌 것이 포함되어 있습니다.")
+
+def isNumber(s):    # 숫자인지 검사
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
