71611c8f60d3783f98dba2a8a6b23a9fea5f24d1
truegen <truegen@gmail.com>
2018-05-07 16:07:04 +0900

    first

---
+++ 2-5.실수의 올림과 내림 처리.py
@@ -0,0 +1,11 @@
+import math
+
+num = 9.5
+
+# 정수형 출력
+print("9.5의 올림값 = %d" %(math.ceil(num)))
+print("9.5이 내림값 = %d" %(math.floor(num)))
+
+# 실수형 출력
+print("9.5의 올림값 = %f" %(math.ceil(num)))
+print("9.5이 내림값 = %f" %(math.floor(num)))
